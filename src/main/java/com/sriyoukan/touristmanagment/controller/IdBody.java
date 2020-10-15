2938f7bbc4e13fdc8d6abe784336003784e67745
nav1996 <2017cs122@stu.ucsc.cmb.ac.lk>
2020-10-15 21:18:46 +0530

    id new

---
+++ src/main/java/com/sriyoukan/touristmanagment/controller/IdBody.java
@@ -0,0 +1,13 @@
+package com.sriyoukan.touristmanagment.controller;
+
+public class IdBody {
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
