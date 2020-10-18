a5dac9c19fdc29074c3132507bb72d26127e529b
virasha96 <57988412+virasha96@users.noreply.github.com>
2020-10-18 09:53:37 +0530

    late changes

---
+++ src/main/java/com/sriyoukan/touristmanagment/controller/RegisterPackBody.java
@@ -0,0 +1,23 @@
+package com.sriyoukan.touristmanagment.controller;
+
+public class RegisterPackBody {
+    private String packId;
+    private String userId;
+
+    public String getPackId() {
+        return packId;
+    }
+
+    public void setPackId(String packId) {
+        this.packId = packId;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+}
+
